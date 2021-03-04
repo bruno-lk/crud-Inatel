e07324a2b9c1c6616ee4f649322587436e503ae7
Bruno Nogueira <brunoncarvalho@outlook.com>
2021-03-04 16:11:21 -0300

    Read by name and Read all

--- src/main/java/com/example/stocks/Controller.java
+++ src/main/java/com/example/stocks/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,7 +50,12 @@ public class Controller {
 	}
 	
 	@GetMapping("/stock")
-	public @ResponseBody Iterable<Stock> getAllStocks() {
+	public @ResponseBody Object getAllStocks(@RequestParam(required = false) String name) {
+
+		if(name != null) {
+			return getStock(name);
+		}
+		
 		return stockRepository.findAll();
 	}
 	
